ca17630149fe1dfa56dc41fb4f669945ebe1378c
SameerVohra <sameer6002.be21@chitkara.edu.in>
2024-03-23 13:49:14 +0530

    Started blog project

---
+++ 12MegaBlog/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+  return (
+    <>
+      <h1 className="text-red-600 text-7xl text-center bg-black p-40">HELLO</h1>{" "}
+    </>
+  );
+}
+
+export default App;
