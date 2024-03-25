84bf33073612beb175d639f9b9313ff904f44ee7
SameerVohra <sameer6002.be21@chitkara.edu.in>
2024-03-25 18:27:10 +0530

    configured redux

--- 12MegaBlog/src/App.jsx
+++ 12MegaBlog/src/App.jsx
@@ -1,10 +1,36 @@
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth.js";
+import { login, logout } from "./store/authSlice.js";
+import { Header, Footer } from "./components/index.js";
+import { Outlet } from "react-router-dom";
 function App() {
   console.log(import.meta.env.VITE_APPWRITE_URL);
-  return (
-    <>
-      <h1 className="text-red-600 text-7xl text-center bg-black p-40">HELLO</h1>{" "}
-    </>
-  );
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-500">
+      <div className="w-full block">
+        <Header />
+        <main>TODO: {/*<Outlet />*/} </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App;
